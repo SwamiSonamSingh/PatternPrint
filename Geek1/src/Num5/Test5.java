0a42c78455f28950667a5b4825de510496e598a6
HP <HP@DESKTOP-MB3IDCM>
2021-01-15 11:43:16 +0530

    15/01/2021

---
+++ Geek1/src/Num5/Test5.java
@@ -0,0 +1,17 @@
+package Num5;
+public class Test5 
+{
+	public static void main(String[] ar)
+	{
+		
+		//printing pattern using non function method
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=6-i;j++)
+			{
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Geek1/src/module-info.java
@@ -0,0 +1,2 @@
+module Geek1 {
+}
